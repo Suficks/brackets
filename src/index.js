5c4740b898e4182e866038bac4ce39a821cd66f9
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-08-08 22:24:34 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openingBrackets = [];
+  const closingBrackets = [];
+  const matchingBrackets = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    const [openingBracket, closingBracket] = bracketsConfig[i];
+    openingBrackets.push(openingBracket);
+    closingBrackets.push(closingBracket);
+    matchingBrackets[closingBracket] = openingBracket;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const currentBracket = str[i];
+
+    if (openingBrackets.includes(currentBracket)) {
+      if (closingBrackets.includes(currentBracket) && stack[stack.length - 1] === currentBracket) {
+        stack.pop();
+      } else {
+        stack.push(currentBracket);
+      }
+    } else if (closingBrackets.includes(currentBracket)) {
+      const lastOpeningBracket = stack.pop();
+      if (matchingBrackets[currentBracket] !== lastOpeningBracket) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
